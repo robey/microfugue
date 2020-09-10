52a264f995d08ff3f7461ad5807e35674cbb6375
Robey Pointer <robeypointer@gmail.com>
2020-09-10 10:49:09 -0700

    add rich text

---
+++ src/test/test_rich_text.ts
@@ -0,0 +1,118 @@
+import { RichText } from "../tfjs/rich_text";
+
+import "should";
+import "source-map-support/register";
+
+describe("RichText", () => {
+  it("flat", () => {
+    const flat = RichText.string("777", "days of abandon");
+
+    flat.at(0).should.eql("d");
+    flat.at(3).should.eql("s");
+    flat.at(6).should.eql("f");
+    flat.at(13).should.eql("o");
+    flat.at(15).should.eql("");
+
+    flat.split(0).map(x => x.toString()).should.eql([ "{777:}", "{777:days of abandon}" ]);
+    flat.split(3).map(x => x.toString()).should.eql([ "{777:day}", "{777:s of abandon}" ]);
+    flat.split(6).map(x => x.toString()).should.eql([ "{777:days o}", "{777:f abandon}" ]);
+    flat.split(13).map(x => x.toString()).should.eql([ "{777:days of aband}", "{777:on}" ]);
+    flat.split(15).map(x => x.toString()).should.eql([ "{777:days of abandon}", "{777:}" ]);
+  });
+
+  it("multi-string", () => {
+    const r = new RichText("777", [ "days ", "of", " abandon" ]);
+
+    r.at(0).should.eql("d");
+    r.at(3).should.eql("s");
+    r.at(6).should.eql("f");
+    r.at(13).should.eql("o");
+    r.at(15).should.eql("");
+
+    r.split(0).map(x => x.toString()).should.eql([ "{777:}", "{777:days of abandon}" ]);
+    r.split(3).map(x => x.toString()).should.eql([ "{777:day}", "{777:s of abandon}" ]);
+    r.split(6).map(x => x.toString()).should.eql([ "{777:days o}", "{777:f abandon}" ]);
+    r.split(13).map(x => x.toString()).should.eql([ "{777:days of aband}", "{777:on}" ]);
+    r.split(15).map(x => x.toString()).should.eql([ "{777:days of abandon}", "{777:}" ]);
+  });
+
+  it("nested", () => {
+    const blue = RichText.string("blue", "of");
+    const r = new RichText("777", [ "days ", blue, " abandon" ]);
+    r.at(0).should.eql("d");
+    r.at(3).should.eql("s");
+    r.at(6).should.eql("f");
+    r.at(13).should.eql("o");
+    r.at(15).should.eql("");
+
+    r.split(0).map(x => x.toString()).should.eql([ "{777:}", "{777:days {blue:of} abandon}" ]);
+    r.split(3).map(x => x.toString()).should.eql([ "{777:day}", "{777:s {blue:of} abandon}" ]);
+    r.split(6).map(x => x.toString()).should.eql([ "{777:days {blue:o}}", "{777:{blue:f} abandon}" ]);
+    r.split(13).map(x => x.toString()).should.eql([ "{777:days {blue:of} aband}", "{777:on}" ]);
+    r.split(15).map(x => x.toString()).should.eql([ "{777:days {blue:of} abandon}", "{777:}" ]);
+  });
+
+  it("deeply nested", () => {
+    const blue = RichText.string("blue", "of");
+    const red = RichText.string("red", "band");
+    const bright = new RichText("888", [ "days ", blue, " a", red ]);
+    const r = new RichText("777", [ bright, "on" ]);
+
+    r.at(0).should.eql("d");
+    r.at(3).should.eql("s");
+    r.at(6).should.eql("f");
+    r.at(13).should.eql("o");
+    r.at(15).should.eql("");
+
+    r.split(0).map(x => x.toString()).should.eql([ "{777:}", "{777:{888:days {blue:of} a{red:band}}on}" ]);
+    r.split(3).map(x => x.toString()).should.eql([ "{777:{888:day}}", "{777:{888:s {blue:of} a{red:band}}on}" ]);
+    r.split(6).map(x => x.toString()).should.eql([ "{777:{888:days {blue:o}}}", "{777:{888:{blue:f} a{red:band}}on}" ]);
+    r.split(13).map(x => x.toString()).should.eql([ "{777:{888:days {blue:of} a{red:band}}}", "{777:on}" ]);
+    r.split(15).map(x => x.toString()).should.eql([ "{777:{888:days {blue:of} a{red:band}}on}", "{777:}" ]);
+  });
+
+  it("parse", () => {
+    RichText.parse("days of abandon", "777").toString().should.eql("{777:days of abandon}");
+    RichText.parse("days {blue:of} abandon", "777").toString().should.eql("{777:days {blue:of} abandon}");
+    RichText.parse("days \\{blue:of} abandon", "777").toString().should.eql("{777:days \\{blue:of\\} abandon}");
+    RichText.parse("days {blue:of abandon", "777").toString().should.eql("{777:days {blue:of abandon}}");
+    RichText.parse("days {of} abandon", "777").toString().should.eql("{777:days \\{of\\} abandon}");
+    RichText.parse("days {of abandon", "777").toString().should.eql("{777:days \\{of abandon}");
+    RichText.parse("days }of abandon", "777").toString().should.eql("{777:days \\}of abandon}");
+    RichText.parse("days {blue:of} a{red:band}on", "777").toString().should.eql("{777:days {blue:of} a{red:band}on}");
+    RichText.parse("days {blue:{red:of} aband}on", "777").toString().should.eql("{777:days {blue:{red:of} aband}on}");
+    RichText.parse("{888:days of abandon}", "777").toString().should.eql("{888:days of abandon}");
+    RichText.parse("{888:days of aband}on", "777").toString().should.eql("{777:{888:days of aband}on}");
+    RichText.parse("{888:days {blue:of} a{red:band}}on", "777").toString().should.eql(
+      "{777:{888:days {blue:of} a{red:band}}on}"
+    );
+    RichText.parse("{888:days {blue:of a{red:band{green:on", "777").toString().should.eql(
+      "{888:days {blue:of a{red:band{green:on}}}}"
+    );
+    RichText.parse("}}}}", "777").toString().should.eql("{777:\\}\\}\\}\\}}");
+    RichText.parse("hello {{name}}", "777").toString().should.eql("{777:hello \\{\\{name\\}\\}}");
+  });
+
+  it("findWordWrap", () => {
+    const text1 = RichText.string("000", "turn off the television");
+    (text1.findWordWrap(50) ?? -1).should.eql(50);
+    (text1.findWordWrap(20) ?? -1).should.eql(13);
+    (text1.findWordWrap(5) ?? -1).should.eql(5);
+
+    const text2 = RichText.string("000", "monosyllabicism");
+    (text2.findWordWrap(10) ?? -1).should.eql(-1);
+    (text2.findWordWrap(8) ?? -1).should.eql(-1);
+
+    // days of abandon
+    const blue = RichText.string("blue", "of");
+    const red = RichText.string("red", "band");
+    const bright = new RichText("888", [ "days ", blue, " a", red ]);
+    const r = new RichText("777", [ bright, "on" ]);
+    (r.findWordWrap(50) ?? -1).should.eql(50);
+    (r.findWordWrap(15) ?? -1).should.eql(15);
+    (r.findWordWrap(14) ?? -1).should.eql(8);
+
+    const r2 = RichText.parse("stop m{blue:onosyl{red:labici{black:cism}}}", "777");
+    (r2.findWordWrap(20) ?? -1).should.eql(5);
+  });
+});
---
+++ src/tfjs/rich_text.ts
@@ -0,0 +1,162 @@
+type Span = RichText | string;
+
+export class RichText {
+  constructor(public color: string, public spans: Span[]) {
+    // pass
+  }
+
+  static string(color: string, s: string): RichText {
+    return new RichText(color, [ s ]);
+  }
+
+  // parse a string with a simple color markup:
+  //   - `{color:...}` will mark a section as being in a color
+  //   - can be nested
+  //   - `\{` avoids this parsing
+  static parse(s: string, defaultColor: string): RichText {
+    let i = 0;
+    const end = s.length;
+    let spans: Span[] = [];
+    const colorStack: string[] = [];
+    const spanStack: Span[][] = [];
+
+    // return the next chunk of text that ends with either the end, or a
+    // start of a new segment
+    const nextText = (quoting: boolean = false) => {
+      const start = i;
+      while (i < end && (s[i] != "{" && s[i] != "}") || quoting) {
+        quoting = (!quoting && s[i] == "\\");
+        i++;
+      }
+      if (start == i) return undefined;
+      return s.slice(start, i);
+    };
+
+    // return the color name in a `{color:` segment, or undefined if it's
+    // not a color segment.
+    const nextColor = () => {
+      const start = i;
+      i++;
+      while (i < end && i - start < 10 && s[i].match(/[A-Za-z0-9]/)) i++;
+      if (s[i] != ":") {
+        // we goofed. probably just a `{string`, or maybe `{{macro}}`.
+        i = start;
+        return undefined;
+      }
+      i++;
+      return s.slice(start + 1, i - 1);
+    };
+
+    const popState = () => {
+      const color = colorStack.pop() ?? "";
+      const span = new RichText(color, spans);
+      spans = spanStack.pop() ?? [];
+      spans.push(span);
+    };
+
+    let quoting = false;
+    while (i < end) {
+      const text = nextText(quoting);
+      quoting = false;
+
+      if (text !== undefined) spans.push(text);
+      if (i == end) continue;
+      if (s[i] == "}") {
+        if (colorStack.length == 0 || spanStack.length == 0) {
+          // spurious `}`
+          quoting = true;
+          continue;
+        }
+        popState();
+        i++;
+      } else {
+        // "{"
+        const color = nextColor();
+        if (color == undefined) {
+          // spurious `{`
+          quoting = true;
+          continue;
+        }
+        colorStack.push(color);
+        spanStack.push(spans);
+        spans = [];
+      }
+    }
+
+    // just assume they "meant" to have trailing `}`
+    while (colorStack.length > 0) popState();
+    // if we only built one span, return it, and ignore the default color
+    if (spans.length == 1 && spans[0] instanceof RichText) return spans[0];
+
+    return new RichText(defaultColor, spans);
+  }
+
+  toString(): string {
+    const quote = (s: Span) => {
+      if (s instanceof RichText) return s.toString();
+      return s.replace(/(?<!\\)\{/g, "\\{").replace(/(?<!\\)\}/g, "\\}");
+    };
+    return `{${this.color}:${this.spans.map(quote).join("")}}`;
+  }
+
+  get length(): number {
+    return this.spans.reduce((sum, x) => sum + x.length, 0);
+  }
+
+  // returns [ span index, offset within span ]
+  private findIndex(n: number): [ number, number ] {
+    let sum = 0;
+    for (let i = 0; i < this.spans.length; i++) {
+      const len = this.spans[i].length;
+      if (n < sum + len) return [ i, n - sum ];
+      sum += len;
+    }
+    // past end of string
+    return [ -1, -1 ];
+  }
+
+  at(n: number): string {
+    const [ index, offset ] = this.findIndex(n);
+    if (index == -1) return "";
+    const r = this.spans[index];
+    return (typeof r === "string") ? r[offset] : r.at(offset);
+  }
+
+  split(n: number): [ RichText, RichText ] {
+    const [ index, offset ] = this.findIndex(n);
+    if (index == -1) return [ this, RichText.string(this.color, "") ];
+    const r = this.spans[index];
+    const [ left, right ] = (typeof r === "string") ?
+      [ r.slice(0, offset), r.slice(offset) ] : r.split(offset);
+    const rLeft = this.spans.slice(0, index);
+    const rRight = this.spans.slice(index + 1);
+    if (left.length > 0) rLeft.push(left);
+    if (right.length > 0) rRight.unshift(right);
+    return [ new RichText(this.color, rLeft), new RichText(this.color, rRight) ];
+  }
+
+  findWordWrap(width: number): number | undefined {
+    let [ index, offset ] = this.findIndex(width);
+    if (index < 0) return width;
+    // our candidate position, overall, so far:
+    let n = width;
+
+    while (true) {
+      const s = this.spans[index];
+      if (typeof s === "string") {
+        while (offset >= 0 && !s[offset].match(/[-\s]/)) offset--, n--;
+        if (offset >= 0) return n + 1;
+      } else {
+        const i = s.findWordWrap(offset);
+        if (i !== undefined) return n - offset + i;
+        n -= offset + 1;
+      }
+
+      if (index == 0) return undefined;
+
+      // continue from the final char in the previous span
+      index--;
+      offset = this.spans[index].length - 1;
+    }
+  }
+}
